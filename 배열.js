c4e9b210b97ca4f521646229a48328bfd14fddf6
Jeongho Park <d0lbangi13@naver.com>
2023-05-22 21:53:46 +0900

    feat: add 배열 part

--- 배열.js
+++ 배열.js
@@ -34,3 +34,57 @@ arr.forEach(element => {
 // console.log(
 //     typeof arr
 // )
+
+////////////////////////////////////////
+
+/**
+ *  # 배열
+ * 1. 객체
+ * 2. index: value (딕셔너리)
+ */
+
+const arr = [1,2,3];
+arr; // [1,2,3]
+
+const arr2 = new Array(1,2,3);
+arr2; // [1,2,3]
+
+arr[0]; // 1
+arr[1]; //2
+arr[2]; // 2
+arr[3] = 4; // 3
+arr.push(5); // 5
+arr[9] = 10; // 10
+
+arr['prop'] = [6,7]; // [6,7] 
+arr.prop; // [6,7]
+
+////////////////////////////////////////
+
+/**
+ * # 배열의 length
+ * 배열에서 가장 높은 인덱스를 추적
+ */
+
+// 배열이 어려운 이유 - 빈 공간도 다 하나의 요소로 포함시키기 때문이다.
+
+const arr = [1,2,3];
+
+arr[9] = undefined; // undefined
+
+arr; // [1,2,3, <6 empty items>, undefined]
+
+arr.length; // 10
+
+arr.forEach(el => console.log(el)); // 1 2 3 undefined undefined
+
+
+// 만약 강제적으로 length의 길이를 늘리면 늘리만큼 숫자가 늘어난다.
+arr.length = 20;
+arr.length; // 20
+arr; // [1,2,3, <6 empty items>, undefined, <10 empty items>]
+
+// 만약 길이를 줄어보면 비어져버렸다. => 불변값이 아니라 참조값이기 때문이다.
+arr.length= 0; // 0
+arr; // []
+
