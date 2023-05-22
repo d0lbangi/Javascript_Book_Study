c2148238cffbd1687ff4393043388fdb2cd7d71f
Jeongho Park <d0lbangi13@naver.com>
2023-05-22 20:11:48 +0900

    feat: add 문자열 리터럴

---
+++ 문자열 리터럴.js
@@ -0,0 +1,22 @@
+/**
+ * 문자열 리터럴
+ */
+
+const singleQuote = '1\
+2\
+3\
+3\
+';
+const doubleQuote = "";
+const backQuote = `1
+2
+3
+4
+5
+`;
+
+const empty = '';
+console.log(empty + 'hello' ); // nullhello
+
+const empty2 = '123';
+console.log(empty2 + 'hello' ); // 123hello
