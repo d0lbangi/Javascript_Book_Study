f28478e7138fc24325fa92f48a5fa617ab784446
Jeongho Park <d0lbangi13@naver.com>
2023-05-22 20:41:02 +0900

    feat: add 문자열과 배열

---
+++ 문자열과 배열.js
@@ -0,0 +1,15 @@
+'HELLO WORLD'.split(''); // ['H','E','L', 'O', '', 'W', 'R', 'L', 'D' ]]
+'HELLO, WORLD'.split(','); // ['Hello', 'WORLD]
+
+['HE', 'LLO', 'WO', 'RLD'].join(''); // 'HELLOWORLD'
+['HE', 'LLO', 'WO', 'RLD'].join(','); // 'HE, LLO, WO, RLD'
+
+const helloWorld = 'HELLO WORLD';
+[...helloWorld]; // ['H','E','L', 'O', '', 'W', 'R', 'L', 'D' ]]
+
+join();
+split();
+
+helloWorld.length; // Array(Reference, Object) // 3
+['1',2,3].length; // primitive // 3
+
